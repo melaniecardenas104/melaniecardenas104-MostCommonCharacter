270954d224788b05420582ed239d48a81b180352
Melanie Cardenas <melaniecardenas19@yahoo.com>
2023-11-01 23:38:21 +0000

    C124 MostCommonCharacter HashMap<char,int> charcountmap = new Hashmap<>(); for loop another for loop

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +9,21 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+
+        for(char c : str.toCharArray()){
+            charCountMap.put(c, charCountMap.getOrDefault(c,0) +1);
+        }
+        int maxCount = 0;
+        char maxChar = 0;
+
+        for(HashMap.Entry<Character, Integer> entry : charCountMap.entrySet()){
+            if(entry.getValue() > maxCount){
+                maxCount = entry.getValue();
+                maxChar = entry.getKey();
+            }
+
+        }
+        return maxChar;
     }
 }
